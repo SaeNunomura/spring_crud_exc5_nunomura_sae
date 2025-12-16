5abf72c77632f5b911c04cab8cfeb62c8b01302c
edu <edu@PC-013-NUNOMURA>
2025-12-16 10:38:09 +0900

    add: 布村 最初のコミット

---
+++ spring_crud_exc5_nunomura_sae/src/main/java/jp/co/sss/crud/controller/IndexController.java
@@ -0,0 +1,61 @@
+package jp.co.sss.crud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.crud.bean.LoginResultBean;
+import jp.co.sss.crud.form.LoginForm;
+import jp.co.sss.crud.service.LoginService;
+
+@Controller
+public class IndexController {
+
+	@Autowired
+	LoginService loginService;
+
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String index(@ModelAttribute LoginForm loginForm) {
+		return "index";
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.POST)
+	public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, Model model,
+			HttpSession sesson) {
+
+		String path = "index";
+
+		if (result.hasErrors()) {
+			LoginResultBean loginResultBean = loginService.execute(loginForm);
+
+			if (loginResultBean.isLogin()) {
+				sesson.setAttribute("loginUser", loginResultBean.getLoginUser());
+				path = "redirect:/list";
+			} else {
+				model.addAttribute("errMessage", loginResultBean.getErrorMsg());
+			}
+
+			return path;
+		} else {
+			path = "redirect:/list";
+			return path;
+		}
+
+		//TODO LoginServiceが完成後にコメントを外す
+
+	}
+
+	@RequestMapping(path = "/logout", method = RequestMethod.GET)
+	public String logout(HttpSession session) {
+
+		session.invalidate();
+		return "redirect:/";
+	}
+
+}
